25023c5bdc2b819e27c7f24bc7002674952d60f4
Eze Sunday Eze <mailsforeze@gmail.com>
2018-10-15 02:42:53 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# A Simple Customized Progressbar for Csharp developers
+
+## Installation
+
+Copy DLL file from bin/Debug diretory. It's named pbar.cs.dll
+
+And place it in your project library folder.
+
+## Usage
+
+```csharp
+using pbarcontrol
+```
+
+You can extend this progressbar to meet your need.
---
+++ pbarcontrol.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+
+namespace pbar.cs
+{
+    public partial class pbarcontrol : UserControl
+    {
+        public pbarcontrol()
+        {
+            InitializeComponent();
+            label1.ForeColor = Color.Black;
+            this.ForeColor = SystemColors.Highlight;// this sets the controls color
+          
+        }
+
+        protected float percent = 0.0f;
+
+        public float Value
+        {
+
+            get
+            {
+                return percent;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    value = 0;
+                }
+                else if(value>100)
+                {
+                    value = 100;
+                  
+                }
+                percent = value;
+                label1.Text = value.ToString() + "%";
+                label1.ForeColor = Color.White;
+                this.Invalidate();
+                    
+            }
+
+           
+        }
+
+
+        private void pbarcontrol_SizeChanged(object sender, EventArgs e)
+        {
+            //maintain the location of the label at the middle
+            label1.Location = new Point(this.Width / 2 - 21 / 2 - 4, this.Height / 2 - 15 / 2);
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Brush b = new SolidBrush(this.ForeColor);
+            LinearGradientBrush ib = new LinearGradientBrush(new Rectangle(0, 0, this.Width, this.Height), Color.FromArgb(255, Color.White), Color.FromArgb(50, Color.White), LinearGradientMode.ForwardDiagonal);
+
+            // calculating the percentage
+
+            int width = (int)((percent / 100) * this.Width);
+            e.Graphics.FillRectangle(b, 0, 0, width, this.Height);
+            e.Graphics.FillRectangle(ib, 0, 0, width, this.Height);
+            b.Dispose();
+            ib.Dispose();
+        }
+
+       
+       
+    }
+}
